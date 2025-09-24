2a7b63a81196c9c4f9964523c738efa4cd4450aa
kj0sh <kingjayeriughe@gmail.com>
2025-09-24 09:52:02 +0100

    feat: import styling and fields from cdn

--- App/views/home.view.php
+++ App/views/home.view.php
@@ -1,11 +1,163 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>HBS Framework</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>HBS Resources</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon ============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+    <!-- master CSS ============================================ -->
+    <link rel="stylesheet" href="https://cdn.hbsresources.org/master.css">
+
+    <!-- Color Css Files Start -->
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-one.css" title="color-one" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-two.css" title="color-two" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-three.css" title="color-three" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-four.css" title="color-four" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-five.css" title="color-five" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-six.css" title="color-six" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-seven.css" title="color-seven" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-eight.css" title="color-eight" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-nine.css" title="color-nine" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-ten.css" title="color-ten" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-eleven.css" title="color-eleven" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/color-twelv.css" title="color-twelve" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/pattren1.css" title="pattren1" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/pattren2.css" title="pattren2" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/pattren3.css" title="pattren3" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/pattren4.css" title="pattren4" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/pattren5.css" title="pattren5" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/background1.css" title="background1" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/background2.css" title="background2" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/background3.css" title="background3" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/background4.css" title="background4" media="screen">
+    <link rel="alternate stylesheet" type="text/css" href="https://cdn.hbsresources.org/switcher/background5.css" title="background5" media="screen">
 </head>
+
 <body>
-	This is the home page
+    <!-- Start main area -->
+    <div class="main-area">
+        <!-- Start header -->
+        <header>
+            <!-- Start header top -->
+            <!-- End header top -->
+            <!-- Start main menu area -->
+            <div class="main-menu-area" id="sticker">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-6">
+                            <div class="logo ptb-32">
+                                <a href="index.html">
+                                    <img src="img/logo.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-9 col-6 position-relative">
+                            <div class="main-menu">
+                                <nav>
+                                    <ul>
+                                        <li><a href="">Home</a></li>
+                                        <li><a href="">About</a></li>
+                                        <li class=""><a href="#">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="donate-button ptb-32">
+                                <!-- <a class="waves-effect waves-light" href="donate.html"></a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="mobile-menu">
+                            <nav id="dropdown">
+                                <ul class="nav">
+                                    <li><a href="">Home</a></li>
+                                    <li><a href="">About</a></li>
+                                    <li class=""><a href="#">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- End footer -->
+    </div>
+
+    <script src="https://cdn.hbsresources.org/js/vendor/modernizr-3.6.0.min.js"></script>
+
+    <!-- jquery-3.6.0.min js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/vendor/jquery-3.6.0.min.js"></script>
+
+    <!-- jquery-migrate-3.3.2.min js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+
+    <!-- bootstrap js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/bootstrap.bundle.min.js"></script>
+
+    <!-- owl.carousel.min js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/owl.carousel.min.js"></script>
+
+    <!-- jquery.nivo.slider.pack js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.nivo.slider.pack.js"></script>
+
+    <!-- jquery.mixitup.min js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.mixitup.min.js"></script>
+
+    <!-- jquery.magnific-popup.min js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.magnific-popup.min.js"></script>
+
+    <!-- jquery.meanmenu js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.meanmenu.js"></script>
+
+    <!-- jquery.syotimer.min js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.syotimer.min.js"></script>
+
+    <!-- wow js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/wow.js"></script>
+
+    <!-- jquery.scrollUp.min js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.scrollUp.min.js"></script>
+
+    <!-- jquery.ajaxchimp.min js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- plugins js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/plugins.js"></script>
+
+    <script>
+        new WOW().init();
+    </script>
+
+    <!-- styleswitch js
+        ============================================ -->
+    <script src="https://cdn.hbsresources.org/js/styleswitch.js"></script>
+
+    <!-- main js
+		============================================ -->
+    <script src="https://cdn.hbsresources.org/js/main.js"></script>
 </body>
+
 </html>
